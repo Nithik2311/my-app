4fd9c36c42a183eeed7b2dd50ee04f2ae93464b0
Nithik2311 <nithikkaladharan@gmail.com>
2025-10-15 23:48:53 +0530

    hello

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,13 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// IMPORTANT: Replace with your actual Firebase config
 const firebaseConfig = {
-
   apiKey: "AIzaSyB5IiXVcDFpbflTpamoHrx-VN5alCgS41g",
   authDomain: "automatedbusschedule.firebaseapp.com",
   projectId: "automatedbusschedule",
@@ -17,10 +12,10 @@ const firebaseConfig = {
   measurementId: "G-RN1LMNRMXH"
 };
 
-
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
 const db = getFirestore(app);
 
 // Test the connection immediately
